b035118c6be8b2c10b828b21d406ba4dac4dece2
miliPaoletti <40247040+miliPaoletti@users.noreply.github.com>
2022-08-17 19:49:07 +0000

    Deploying to gh-pages from @ miliPaoletti/test-githubpages-ts@3cbcfbbfcc6bb871dbf6a9cb2f19fd3bb5411b66 🚀

---
+++ _next/static/SBNsuwcizZOLM1yW2ht-y/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/7112840a-088dca16b8392537.js","static/chunks/596-270519a5cff091ca.js","static/chunks/pages/index-aaf3e4bae97ffad9.js"],"/_error":["static/chunks/pages/_error-a4ba2246ff8fb532.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
